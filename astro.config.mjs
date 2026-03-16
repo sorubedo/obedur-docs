14bc35f3804fcd0942a6efee71e3ccd6fb12bd98
sorubedo <zhanrenhangg@gmail.com>
2026-03-16 17:53:13 +0800

    docs: setup github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://sorubedo.github.io',
+	base: '/obedur-docs',
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'obedur-os Docs',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
